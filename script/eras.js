0a2e8a1734e8f19a20448674c4a220ef5985d376
VilcaLautaro <lavilca@alumno.unlam.edu.ar>
2024-06-19 17:26:42 -0300

    update terminado

---
+++ script/eras.js
@@ -0,0 +1,176 @@
+const TTPD = {nombre: "The Tortured Poets Department", 
+    genero: "Synth Pop - Folk Pop", 
+    productores: [{nombreProd: "Taylor Swift", productorLink: "https://es.wikipedia.org/wiki/Taylor_Swift"}, 
+                {nombreProd: "Jack Antonoff", productorLink: "https://es.wikipedia.org/wiki/Jack_Antonoff"}],
+    canciones: [{nombreCancion:"Fortnight", linkMV:"https://youtu.be/q3zqJs7JUCQ?si=wxOoxVzRQ9B5wLs1", linkLV:"https://youtu.be/HzsQHfBA3MY?si=WnVz0ZGhov6Mu-PS"},
+                {nombreCancion:"The Tortured Poets Department", linkMV:"https://youtu.be/Yy1UNxI-Z9s?si=dOy3nksjIrZ6gV8q", linkLV:"https://youtu.be/RQMz4JDbtmI?si=N_b6n-C-EpkKQUF2"},
+                {nombreCancion:"My Boy Only Breaks His Favorite Toys", linkMV:"https://youtu.be/pt_dyO-17xw?si=DG2NRFBI3pJWS99X", linkLV:"https://youtu.be/wRKXAAV6jh4?si=uAh6t9fz-h2_UG6H"},
+                {nombreCancion:"Down Bad", linkMV:"https://youtu.be/IGjXBhnj_H0?si=AcO_xCV0R589-VRA", linkLV:"https://youtu.be/EVbtjaWXQVg?si=N303J0hlwmI3Ffly"},
+                {nombreCancion:"So Long, London", linkMV:"https://youtu.be/mSTdxnVqhpc?si=bmvtehGw-TlN-Vq_", linkLV:"https://youtu.be/CCUr2pNJft4?si=6JAOxH9q4zBv_Sxi"}
+    ],
+    descripcion: "The Tortured Poets Department se adentra en las complejidades del amor y la pérdida. A lo largo de 16 canciones, explora el viaje emocional del desamor, la sanación y el crecimiento personal. Inspirado en una ruptura dolorosa, el álbum retrata la vulnerabilidad y la fuerza que surgen al navegar la tristeza, la ira y, finalmente, encontrar la paz.", 
+    imagen: "../Assets/home/eras/1-ttpd_portada.jpg", 
+    iframe:  "https://www.youtube.com/embed/hVdsAbmKiy0?si=GQ8IjPtTbCu_IIO2"}
+
+const MIDNIGHTS = {nombre: "Midnights",
+    genero: "Dream Pop - Electro Pop", 
+    productores: [{nombreProd: "Taylor Swift", productorLink: "https://es.wikipedia.org/wiki/Taylor_Swift"}, 
+                {nombreProd: "Jack Antonoff", productorLink: "https://es.wikipedia.org/wiki/Jack_Antonoff"}],
+    canciones: [{nombreCancion: "Lavender Haze", linkMV:"https://youtu.be/h8DLofLM7No?si=kEjALcdo41g551d7", linkLV:"https://youtu.be/mkR_Qwix4Ho?si=6BY2DGWHyiQU7S8F"},
+                {nombreCancion: "Maroon", linkMV:"https://youtu.be/Qs0Yrzza6ng?si=bbOyoKmmeVe6hDrj", linkLV:"https://youtu.be/lvHZjvIyqsk?si=TdoApU5eB0YropWA"},
+                {nombreCancion: "Anti-Hero", linkMV:"https://youtu.be/b1kbLwvqugk?si=X3VcTmDcv-zekNpb", linkLV:"https://youtu.be/XqN2qFvY64U?si=n3aQwrD11okAlRbt"},
+                {nombreCancion: "Snow On The Beach", linkMV:"https://youtu.be/2CnUYMmEHrs?si=sfpALizwljnKw_pt", linkLV:"https://youtu.be/ycE7bUq3-2k?si=REaNWF4lYvmSmZZc"},
+                {nombreCancion: "You're On Your Own, Kid", linkMV:"https://youtu.be/RaZRoX-jEGM?si=nCbzlIe1eQ3Rc7-1", linkLV:"https://youtu.be/7Gbg6Z70J7E?si=ljRO7VIKWbgCfAFR"},
+    ],
+    descripcion: "Midnights de Taylor Swift, inspirado en 13 noches de insomnio, adopta una estética de los años 70 y experimenta con estilos como el synth-pop y el dream pop. Con letras confesionales y crípticas, aborda la autocrítica, la seguridad, la inseguridad, la ansiedad y el insomnio.", 
+    imagen: "../Assets/home/eras/2-midnights_portada.jpg", 
+    iframe: "https://www.youtube.com/embed/h8DLofLM7No?si=lsGgtJLQPJriZcam"}
+
+const EVERMORE = {nombre: "Evermore", 
+    genero: "Rock - Chamber Rock - Pop - Folk", 
+    productores: [{nombreProd: "Taylor Swift", productorLink: "https://es.wikipedia.org/wiki/Taylor_Swift"}, 
+                {nombreProd: "Jack Antonoff", productorLink: "https://es.wikipedia.org/wiki/Jack_Antonoff"}],
+    canciones: [{nombreCancion:"willow", linkMV: "https://youtu.be/RsEZmictANA?si=Uj-MA0cwHuzwzHqg", linkLV: "https://youtu.be/7EvwIw4gIyk?si=_K0aigASStzVEOvg"},
+                {nombreCancion:"champagne problems", linkMV: "https://youtu.be/g9aNdMLYUrk?si=5j3UoES5OBboX48O", linkLV: "https://youtu.be/wMpqCRF7TKg?si=2J-zLJxqUldQ9QTg"},
+                {nombreCancion:"gold rush", linkMV: "https://youtu.be/4WpeIIq_e_Y?si=LI4gRGAP69OoQ6WL", linkLV: "https://youtu.be/Pz-f9mM3Ms8?si=IEx0I6WM3lDWoCFM"},
+                {nombreCancion:"'tis the damn season", linkMV: "https://youtu.be/URHdBhdFiQY?si=Dm9oquoF2qOhgeUr", linkLV: "https://youtu.be/WuvhOD-mP8M?si=2SiFyMk2UpjmkPvm"},
+                {nombreCancion:"tolerate it", linkMV: "https://youtu.be/_Crn-zgnErk?si=HEVOF6iM-sOBOkit", linkLV: "https://youtu.be/ukxEKY_7MOc?si=JUtAvy0AtwJ0x6fe"}
+    ],
+    descripcion: "Evermore de Taylor Swift, complemento de Folklore, explora temas de amor, infidelidad y dolor en tercera persona. Con colaboraciones de Bon Iver, Haim y The National, fue aclamado por su carácter y producción experimental, apareciendo en muchas listas de los mejores álbumes de 2020.", 
+    imagen: "../Assets/home/eras/3-evermore_portada.jpg", 
+    iframe: "https://www.youtube.com/embed/Il6cMQnnS6c?si=IUtDt2fpDoGtDx-w"}
+
+const FOLKLORE= {nombre: "Folklore", 
+    genero: "Rock - Folk - Electrofolk", 
+    productores: [{nombreProd: "Taylor Swift", productorLink: "https://es.wikipedia.org/wiki/Taylor_Swift"}, 
+                  {nombreProd: "Jack Antonoff", productorLink: "https://es.wikipedia.org/wiki/Jack_Antonoff"}],
+    canciones: [{nombreCancion:"the 1", linkMV: "https://youtu.be/EGOX82ZdFxg?si=v1qZmiCxzkI5Z8J1", linkLV: "https://youtu.be/KsZ6tROaVOQ?si=OHk31_k6bfAz_27o"},
+                {nombreCancion:"cardigan", linkMV: "https://youtu.be/K-a8s8OLBSE?si=U9nvAotYz120SU2G", linkLV: "https://youtu.be/zLSUp53y-HQ?si=zQwQw26_X1Y5OIg2"},
+                {nombreCancion:"the last great american dynasty", linkMV: "https://youtu.be/5dOAUYf_Ksk?si=P3zTjAtvtwfaZERH", linkLV: "https://youtu.be/2s5xdY6MCeI?si=BOI_GVjFRVCXpAgg"},
+                {nombreCancion:"exile", linkMV: "https://youtu.be/o5SQIECedTY?si=-2Km8Tv8zdsE7HW5", linkLV: "https://youtu.be/osdoLjUNFnA?si=h1nqZQc9ny66kLnh"},
+                {nombreCancion:"my Tears Ricochet", linkMV: "https://youtu.be/Y5lbSP1uwMk?si=Of9SZabNS4tNvyVh", linkLV: "https://youtu.be/OWbDJFtHl3w?si=yuWnO3conz0CcLX5"}
+        ],
+    descripcion: "Apartándose de temas autobiográficos, Taylor Swift usa una narrativa en tercera persona en Folklore, creando historias y personajes ficticios. El álbum explora el escapismo, la nostalgia, la soledad y la introspección, reflejados en su estética natural.", 
+    imagen: "../Assets/home/eras/4-folklore_portada.jpg", 
+    iframe: "https://www.youtube.com/embed/tf0H05K73BU?si=Kv_soRS60-GALA0m"}
+
+const LOVER = {nombre: "Lover", 
+    genero: "Pop - Pop Rock - Electro Pop", 
+    productores: [{nombreProd: "Taylor Swift", productorLink: "https://es.wikipedia.org/wiki/Taylor_Swift"}, 
+                  {nombreProd: "Jack Antonoff", productorLink: "https://es.wikipedia.org/wiki/Jack_Antonoff"}],
+    canciones: [{nombreCancion:"I Forgot That You Existed", linkMV: "https://youtu.be/p1cEvNn88jM?si=RxHweAtE2GL0IQrB", linkLV: "https://youtu.be/KCIKbE_N_DU?si=DTnmzHqj7ep_iWrx"},
+                {nombreCancion:"Cruel Summer", linkMV: "https://youtu.be/ic8j13piAhQ?si=3sFB99mvkEFjw37O", linkLV: "https://youtu.be/P8T1rUpVdXE?si=iWQKgXtE0x66GeoK"},
+                {nombreCancion:"Lover", linkMV: "https://youtu.be/-BjZmE2gtdo?si=Xzy7yRY59wfYVxUR", linkLV: "https://youtu.be/cvUAzpn48xA?si=p6pqmZzUcJBtvtdQ"},
+                {nombreCancion:"The Man", linkMV: "https://youtu.be/AqAJLh9wuZ0?si=UJP2gIDqHIjlp4Mp", linkLV: "https://youtu.be/tbEekLA7J3Y?si=9XPF_0mj2j7jF7Hr"},
+                {nombreCancion:"The Archer", linkMV: "https://youtu.be/mXTO2VI0GFQ?si=PuGuj6mM9SGfc0TE", linkLV: ""}
+        ],
+    descripcion: "Lover es el séptimo álbum de estudio de Taylor Swift, lanzado en 2019. Se caracteriza por un estilo pop, pop rock y electro pop, explorando temas de amor y romance con una sensibilidad optimista y melódica.", 
+    imagen: "../Assets/home/eras/5-lover_portada.jpg", 
+    iframe: "https://www.youtube.com/embed/-BjZmE2gtdo?si=x9Y5zgjEfJ_QV4MS"};
+
+const REPUTATION = {nombre: "Reputation", 
+    genero: "Electropop - Dance Pop - Pop", 
+    productores: [{nombreProd: "Taylor Swift", productorLink: "https://es.wikipedia.org/wiki/Taylor_Swift"}, 
+                  {nombreProd: "Jack Antonoff", productorLink: "https://es.wikipedia.org/wiki/Jack_Antonoff"}],
+    canciones: [{nombreCancion:"…ready for it?", linkMV: "https://youtu.be/wIft-t-MQuE?si=rTk2eDrUELPo9YgA", linkLV: "https://youtu.be/5wQUXFnyqiI?si=whUghB0qD7EmvFhN"},
+                {nombreCancion:"End Game", linkMV: "https://youtu.be/dfnCAmr569k?si=I-L05W2TM_txZVd9", linkLV: "https://youtu.be/zjOddwR4nqw?si=h9LBcdEneskqWAYL"},
+                {nombreCancion:"I Did Something Bad", linkMV: "https://youtu.be/xYLxUJ9v6KU?si=EkZ0McY4P98YETPe", linkLV: "https://youtu.be/Sv_I15_kZHw?si=hiyOKCgRjppVUKVK"},
+                {nombreCancion:"Don't Blame Me", linkMV: "https://youtu.be/kRJKB291Z1g?si=qBRWMX_NrHM5h0xc", linkLV: "https://youtu.be/Os0mdp3pX7U?si=yjpqs3ik2EzhN9Rb"},
+                {nombreCancion:"Delicate", linkMV: "https://youtu.be/tCXGJQYZ9JA?si=jk0GXLn44uafLk4l", linkLV: "https://youtu.be/nxM67sLvGGU?si=FraEHGkb9Qxi0M9G"}
+        ],
+    descripcion: "Reputation es el sexto álbum de estudio de Taylor Swift, lanzado en 2017. Se centra en el electropop y dance pop, explorando temas de fama, amor y autocrítica. Influenciado por la cultura pop, destaca por su producción sofisticada y letras introspectivas que reflexionan sobre la percepción pública de Swift.", 
+    imagen: "../Assets/home/eras/6-reputation_portada.jpg", 
+    iframe: "https://www.youtube.com/embed/WnnWXuiqDJE?si=ogRJ2dtZWIYpIL-V"};
+
+const THE1989 = {nombre: "1989", 
+    genero: "Synth Pop", 
+    productores: [{nombreProd: "Taylor Swift", productorLink: "https://es.wikipedia.org/wiki/Taylor_Swift"}, 
+                  {nombreProd: "Max Martin", productorLink: "https://es.wikipedia.org/wiki/Max_Martin"}],
+    canciones: [{nombreCancion: "Welcome to New York", linkMV: "https://youtu.be/RvYysauPsMc?si=lLzlze-PP778Pb1F", linkLV:"https://youtu.be/FsGdznlfE2U?si=etGIMW2oEyrz5QYX"},
+                {nombreCancion: "Blank Space", linkMV: "https://youtu.be/e-ORhEE9VVg?si=AoN0xHqQKVfk5FZP", linkLV:"https://youtu.be/gir8BEqAutk?si=lzzdzMVNU7QpBUBo"},
+                {nombreCancion: "Style", linkMV: "https://youtu.be/-CmadmM5cOk?si=CFyn6pDs8_B5TfMA", linkLV:"https://youtu.be/2JgvVfOfoWI?si=wgIh0W9cxZlpJgHU"},
+                {nombreCancion: "Out of the Woods", linkMV: "https://youtu.be/JLf9q36UsBk?si=RZydjUG1c2y0g8r2", linkLV:"https://youtu.be/gp1MR2WAAXY?si=1NQA_xgYbB8gP2Ev"},
+                {nombreCancion: "All You Had to Do Was Stay", linkMV: "https://youtu.be/jZ3VMl-uvFE?si=YP_yjgrxiuug2GBc", linkLV:"https://youtu.be/_Usd5X5XJXM?si=PWf2HrCev6HrqiYN"}
+        ],
+    descripcion: "1989 es el quinto álbum de estudio de Taylor Swift, lanzado en 2014. Este álbum marca un cambio hacia el synth pop, destacándose por su producción pulida y pegadizas melodías pop. Las letras reflejan experiencias personales de Swift y exploran temas de amor, nostalgia y crecimiento personal.", 
+    imagen: "../Assets/home/eras/7-1989_portada.jpeg", 
+    iframe: "https://www.youtube.com/embed/_k2VVetoe1Q?si=bOUVgnG8nxn3gNbX" };
+
+const RED = {nombre: "Red", 
+    genero: "Pop - Country - Rock", 
+    productores: [{nombreProd: "Taylor Swift", productorLink: "https://es.wikipedia.org/wiki/Taylor_Swift"}, 
+                  {nombreProd: "Jack Antonoff", productorLink: "https://es.wikipedia.org/wiki/Jack_Antonoff"}],
+    canciones: [{nombreCancion: "State of Grace", linkMV: "https://youtu.be/vTXOY6-ebF0?si=8QYwU_mRGTDHHTeu", linkLV:"https://youtu.be/-mrC5tRkxrY?si=jgTgD9yqaWW6mDeq"},
+                {nombreCancion: "Red", linkMV: "https://youtu.be/Zlot0i3Zykw?si=47N6MgUU9GugawbT", linkLV:"https://youtu.be/R_rUYuFtNO4?si=SKSKH5zXCZGbK8r8"},
+                {nombreCancion: "Treacherous", linkMV: "https://youtu.be/OhAxmGQkcR8?si=QJV53t-MY0cm7eiw", linkLV:"https://youtu.be/u1D1AgDfreg?si=JpttGiW8OgwBFLvu"},
+                {nombreCancion: "I Knew You Were Trouble", linkMV: "https://youtu.be/vNoKguSdy4Y?si=LL_Lr5eOkvpJ4vy-", linkLV:"https://youtu.be/TqAollrUJdA?si=wfl1GeBuAYTdRkYI"},
+                {nombreCancion: "All Too Well", linkMV: "https://youtu.be/a7k7LBp-r5U?si=YIn5tcWOTT-hQkub", linkLV:"https://youtu.be/BZ-rLBkUZf4?si=dVkjaeaOvzxKswKS"}
+            ],                
+descripcion: "Red es el cuarto álbum de estudio de Taylor Swift, lanzado en 2012. Combina elementos de pop, country y rock, mostrando una evolución en el estilo musical de Swift hacia un sonido más maduro y diversificado. Las letras exploran temas de amor, corazones rotos y la complejidad de las relaciones personales.", 
+    imagen: "../Assets/home/eras/8-red_portada.jpg", 
+    iframe: "https://www.youtube.com/embed/Iyua_4FCwVQ?si=0q2xVJPEp9tko-Uu" };
+
+const SPEAKNOW = {nombre: "Speak Now", 
+    genero: "Country Pop - Pop Rock", 
+    productores: [{nombreProd: "Taylor Swift", productorLink: "https://es.wikipedia.org/wiki/Taylor_Swift"}, 
+                  {nombreProd: "Nathan Chapman", productorLink: "https://en.wikipedia.org/wiki/Nathan_Chapman_(record_producer)"}],
+    canciones: [{nombreCancion: "Mine", linkMV: "https://youtu.be/XPBwXKgDTdE?si=KWRIibaiI-S4Q5-g", linkLV:"https://youtu.be/oxNLRqMJMZk?si=__fqNZVxg6rFWdSB"},
+                {nombreCancion: "Sparks Fly", linkMV: "https://youtu.be/oKar-tF__ac?si=dboldETysx2PsOec", linkLV:"https://youtu.be/UlFrV5GJA_4?si=3QCQySD4p414fWMk"},
+                {nombreCancion: "Back to December", linkMV: "https://youtu.be/QUwxKWT6m7U?si=BY673hSpFWQxmXK2", linkLV:"https://youtu.be/qc2Z-OX9wnc?si=Q9AK2uvfyCFclMQp"},
+                {nombreCancion: "Speak Now", linkMV: "https://youtu.be/4PnlKsgpopw?si=eHJGq3i-pRrI2Hfi", linkLV:"https://youtu.be/JlZnvyBqceY?si=yWCWNSapqjZ8Xtx0"},
+                {nombreCancion: "Dear John", linkMV: "https://youtu.be/2tq1m0vU-m4?si=7bhjjtTxNWxx9i0_", linkLV:"https://youtu.be/N-FYySSy0rM?si=8SQNVzcTuHs2tBR9"}
+            ],                
+    descripcion: "Speak Now es el tercer álbum de estudio de Taylor Swift, lanzado en 2010. Caracterizado por su estilo country pop y pop rock, el álbum es conocido por sus letras narrativas y confesionales. Explora temas de amor, desamor y crecimiento personal, mostrando la habilidad de Swift para contar historias a través de su música.", 
+    imagen: "../Assets/home/eras/9-speaknow_portada.jpg", 
+    iframe: "https://www.youtube.com/embed/5jKcG86bNRs?si=i9MgzwPA0pVOJj3G" };
+
+const FEARLESS = {nombre: "Fearless", 
+    genero: "Country Pop", 
+    productores: [{nombreProd: "Taylor Swift", productorLink: "https://es.wikipedia.org/wiki/Taylor_Swift"},
+                  {nombreProd: "Nathan Chapman", productorLink: "https://en.wikipedia.org/wiki/Nathan_Chapman_(record_producer)"}],
+    canciones: [{nombreCancion: "Fearless", linkMV: "https://youtu.be/ptSjNWnzpjg?si=3MLpAoDbvRzTF-oc", linkLV:"https://youtu.be/7lLigiVgJsE?si=TDlTr5p0TZ_ttp4s"},
+                {nombreCancion: "Fifteen", linkMV: "https://youtu.be/Pb-K2tXWK4w?si=6a082m_2k9cOl-nJ", linkLV:"https://youtu.be/rLCol1C3ouc?si=cXkJXSk-NyKsQLnQ"},
+                {nombreCancion: "Love Story", linkMV: "https://youtu.be/8xg3vE8Ie_E?si=rzdS9HRhb80HTZP-", linkLV:"https://youtu.be/aXzVF3XeS8M?si=iaBctgTMjThN4AC9"},
+                {nombreCancion: "Hey Stephen", linkMV: "https://youtu.be/KQNZLnVjT6w?si=WXroAe02F8UtbYcc", linkLV:"https://youtu.be/tMhiHrL7rPE?si=0BzRRmsZwfPUKl_x"},
+                {nombreCancion: "White Horse", linkMV: "https://youtu.be/D1Xr-JFLxik?si=Y2Ooav-DMDPeZNSe", linkLV:"https://youtu.be/9-rKvhsjwKU?si=3ED8XfgyVL7qR7WV"}
+            ], 
+    descripcion: "Fearless es el segundo álbum de estudio de Taylor Swift, lanzado en 2008. Con un estilo predominantemente country pop, el álbum explora temas de amor adolescente, desamor y sueños juveniles. Destacado por sus letras emotivas y pegadizas melodías, Fearless consolidó a Swift como una importante figura en la música pop y country.", 
+    imagen: "../Assets/home/eras/10-fearless_portada.jpg", 
+    iframe: "https://www.youtube.com/embed/SMW4rLMC6cs?si=3JnFZQJvnwIjpM1I" };
+
+const TS = {nombre: "Taylor Swift", 
+    genero: "Pop Rock", 
+    productores: [{nombreProd: "Rob Ellis", productorLink: "https://en.wikipedia.org/wiki/Rob_Ellis_(producer)"},
+                  {nombreProd: "Nathan Chapman", productorLink: "https://en.wikipedia.org/wiki/Nathan_Chapman_(record_producer)"}],
+    canciones: [{nombreCancion: "Tim McGraw", linkMV: "https://youtu.be/GkD20ajVxnY?si=ToqmVJHfnfb-HDFZ", linkLV:"https://youtu.be/KawuwB9p90M?si=yVKsnmGTP6_ItNBD"},
+                {nombreCancion: "Picture to Burn", linkMV: "https://youtu.be/yCMqcFAigRg?si=bfql4B3xOHxUI0iD", linkLV:"https://youtu.be/PKoqTNT4JEM?si=-2Tlhs9bcCd_cun2"},
+                {nombreCancion: "Teardrops on My Guitar", linkMV: "https://youtu.be/xKCek6_dB0M?si=8QGkWe6y9Im8XPfy", linkLV:"https://youtu.be/McYXrhwilmI?si=rPro6T0UEp624s9P"},
+                {nombreCancion: "A Place in This World", linkMV: "https://youtu.be/pYFqLzFoHo0?si=jFubP0k3L3E_c2CA", linkLV:"https://youtu.be/fgZ2q0yyUGA?si=720jT5QIzvQEsJuN"},
+                {nombreCancion: "Cold as You", linkMV: "https://youtu.be/SoXPvCcFibo?si=92fd30vY0FS0b2to", linkLV:"https://youtu.be/PIDklLmalto?si=mV7kyFAOKgbTZCdJ"}
+            ],
+    descripcion: "Taylor Swift es el álbum debut homónimo de Taylor Swift, lanzado en 2006. Con un estilo country y pop rock, el álbum aborda temas de amor juvenil, desamor y la vida adolescente. Sus letras sinceras y melodías pegajosas presentaron a Swift como una prometedora nueva voz en la música country.", 
+    imagen: "../Assets/home/eras/11-taylorswift_portada.jpg", 
+    iframe: "https://www.youtube.com/embed/GkD20ajVxnY?si=__rp2YRJShS3nGo_"};
+
+
+const ERAS = [TTPD, MIDNIGHTS, EVERMORE, FOLKLORE, LOVER, REPUTATION, THE1989, RED, SPEAKNOW, FEARLESS, TS]
+
+
+function mostrarEras() {
+    let nodoEras = document.getElementById("erasMostrar")
+
+    for(let eras of ERAS){
+        let nodoA = document.createElement('a');
+        nodoA.href = `./erasDetails.html?nombre=${eras['nombre']}`;
+        nodoA.target = "_blank";
+        let nodoIMG = document.createElement('img');
+        nodoIMG.src = eras.imagen;
+        nodoA.appendChild(nodoIMG);
+        nodoEras.appendChild(nodoA)
+
+
+    }
+}
+mostrarEras()
+localStorage.setItem('eras', JSON.stringify(ERAS));
+
---
+++ script/erasDetails.js
@@ -0,0 +1,87 @@
+const QUERY_PARAMS_NOMBRE = 'nombre';
+const urlVista = document.location.href;
+const url = new URL(urlVista);
+const nombreEra = url.searchParams.get(QUERY_PARAMS_NOMBRE);
+
+const ERAS = JSON.parse(localStorage.getItem('eras'));
+
+if (ERAS) {
+    // Buscar la era por su nombre
+    const eraData = ERAS.find(era => era.nombre === nombreEra);
+
+    if (eraData) {
+        const era = new Era(eraData);
+
+        document.getElementById('eraTitulo').textContent = era.nombre;
+        document.querySelector('iframe').src = era.iframe;
+        document.getElementById('eraGenero').textContent = era.genero;
+        document.getElementById('eraResumen').textContent = era.descripcion;
+
+        function rellenarProductores(){
+        const productoresContainer = document.querySelector('.nombresProductor');
+        productoresContainer.innerHTML = ''; // Saca por las dudas los productores anteriores
+        const productores = era.productores;
+        productores.forEach(productor => {
+            const link = document.createElement('a');
+            link.href = productor.productorLink;
+            link.textContent = productor.nombreProductor;
+            link.target = '_blank';
+            productoresContainer.appendChild(link);
+        })};
+
+        function rellenarCanciones() {
+            const cancionSelect = document.getElementById('cancion');
+            cancionSelect.innerHTML = ''; // Saca las las opciones anteriores
+
+            const canciones = era.canciones;
+            canciones.forEach(cancion => {
+                const optionElement = document.createElement('option');
+                optionElement.value = cancion.nombre;
+                optionElement.textContent = cancion.nombre;
+                cancionSelect.appendChild(optionElement);
+            });
+
+            // Inicializa el primer video
+            if (canciones.length > 0) {
+                handleCancionChange(); 
+            }
+        }
+
+        function handleCancionChange() {
+            const cancionSelect = document.getElementById('cancion');
+            const selectedSongName = cancionSelect.value;
+            const selectedSong = era.canciones.find(cancion => cancion.nombre === selectedSongName);
+            const videoLink = document.getElementById('linkVideoReproducir');
+
+            /*console.log('Selected Song:', selectedSong); // Log testeo*/
+
+            if (selectedSong) {
+                const formatoSeleccionado = document.getElementById('tiporeproduccion').value;
+                if (formatoSeleccionado === 'opcion1') {
+                    videoLink.href = selectedSong.linkMV;
+                    /*console.log(`MV link loaded: ${selectedSong.linkMV}`); // Log testeo*/
+                } else if (formatoSeleccionado === 'opcion2') {
+                    videoLink.href = selectedSong.linkLyric;
+                    /*console.log(`Lyric link loaded: ${selectedSong.linkLyric}`); // Log testeo*/
+                } else {
+                    console.error('Formato no reconocido:', formatoSeleccionado);
+                }
+            } else {
+                videoLink.href = '';
+            }
+        }
+
+        rellenarCanciones();
+        rellenarProductores();
+
+        const cancionSelect = document.getElementById('cancion');
+        cancionSelect.addEventListener('change', handleCancionChange);
+
+        const formatoSelect = document.getElementById('tiporeproduccion');
+        formatoSelect.addEventListener('change', handleCancionChange);
+    } else {
+        console.error('No se encontro era con este nombre:', nombreEra);
+    }
+} else {
+    console.error('No se encontro informacion en LocalStorage.');
+}
